674b53a6a751bea410a534075dc9aa863105b2d5
Imran Ismail <imran.codely@gmail.com>
2017-01-17 11:36:14 +0800

    Initial commit

---
+++ lib/type.ex
@@ -0,0 +1,18 @@
+defmodule Type do
+  @moduledoc """
+  Documentation for Type.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Type.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/type_test.exs
@@ -0,0 +1,8 @@
+defmodule TypeTest do
+  use ExUnit.Case
+  doctest Type
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
