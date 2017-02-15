3dc7fb152efc25e7cb02ed21d7fc27e8f6758d3f
Imran Ismail <imran.codely@gmail.com>
2017-02-15 16:00:19 +0800

    Update tags.ex

--- lib/type/tags.ex
+++ lib/type/tags.ex
@@ -1,60 +1,58 @@
-if Code.ensure_compiled?(Ecto.Type) do
-  defmodule Type.Tags do
-    @moduledoc """
-    Provides custom Ecto type to use comma delimited tag list.
-    It might work with different adapters, but it has only been tested
-    on PostgreSQL with nested type
-    ## Usage:
+defmodule Type.Tags do
+  @moduledoc """
+  Provides custom Ecto type to use comma delimited tag list.
+  It might work with different adapters, but it has only been tested
+  on PostgreSQL with nested type
+  ## Usage:
 
-    Schema:
-        defmodule Post do
-          use Ecto.Schema
-          schema "posts" do
-            field :title, :string
-            field :body, :string
-            field :tags, Data.TagType
-          end
+  Schema:
+      defmodule Post do
+        use Ecto.Schema
+        schema "posts" do
+          field :title, :string
+          field :body, :string
+          field :tags, Data.TagType
         end
-    Migration:
-        def change do
-          create table(:posts) do
-            add :title, :string
-            add :body, :string
-            add :tags, {:array, :string}
-          end
-        end
-    """
-    @behaviour Ecto.Type
-
-    @spec type() :: {atom, atom}
-    def type, do: {:array, :string}
-
-    @spec cast(list(String.t) | String.t) :: {atom, list(String.t)}
-    def cast(term)
-    def cast(term) when is_list(term), do: {:ok, term}
-    def cast(""), do: {:ok, []}
-    def cast(term) when is_binary(term) do
-      case get_delimiter(term) do
-        :error -> :error
-        delimeter -> {:ok, String.split(term, delimeter, trim: true)}
       end
+  Migration:
+      def change do
+        create table(:posts) do
+          add :title, :string
+          add :body, :string
+          add :tags, {:array, :string}
+        end
+      end
+  """
+  @behaviour Ecto.Type
+
+  @spec type() :: {atom, atom}
+  def type, do: {:array, :string}
+
+  @spec cast(list(String.t) | String.t) :: {atom, list(String.t)}
+  def cast(term)
+  def cast(term) when is_list(term), do: {:ok, term}
+  def cast(""), do: {:ok, []}
+  def cast(term) when is_binary(term) do
+    case get_delimiter(term) do
+      :error -> :error
+      delimeter -> {:ok, String.split(term, delimeter, trim: true)}
     end
-    def cast(_), do: :error
+  end
+  def cast(_), do: :error
 
-    @spec load(list(String.t)) :: {atom, list(String.t)}
-    def load(term) when is_list(term), do: {:ok, term}
+  @spec load(list(String.t)) :: {atom, list(String.t)}
+  def load(term) when is_list(term), do: {:ok, term}
 
-    @spec dump(list(String.t)) :: {atom, list(String.t)}
-    def dump(term) when is_list(term), do: {:ok, term}
-    def dump(_), do: :error
+  @spec dump(list(String.t)) :: {atom, list(String.t)}
+  def dump(term) when is_list(term), do: {:ok, term}
+  def dump(_), do: :error
 
-    @spec get_delimiter(String.t) :: String.t | atom
-    defp get_delimiter(term) do
-      cond do
-        term =~ ~r/^(\w+)(,\s\w+)*$/ -> ", "
-        term =~ ~r/^(\w+)(,\w+)*$/ -> ","
-        true -> :error
-      end
+  @spec get_delimiter(String.t) :: String.t | atom
+  defp get_delimiter(term) do
+    cond do
+      term =~ ~r/^(\w+)(,\s\w+)*$/ -> ", "
+      term =~ ~r/^(\w+)(,\w+)*$/ -> ","
+      true -> :error
     end
   end
 end
