db5ee8b0b57bf5e5ae611a2276fd25e4e184b560
Imran Ismail <imran.codely@gmail.com>
2017-02-15 15:59:54 +0800

    Update external_id.ex

--- lib/type/external_id.ex
+++ lib/type/external_id.ex
@@ -1,22 +1,20 @@
-if Code.ensure_compiled?(Ecto.Type) do
-  defmodule Type.ExternalID do
-    @behaviour Ecto.Type
+defmodule Type.ExternalID do
+  @behaviour Ecto.Type
 
-    @spec type() :: atom
-    def type, do: :string
+  @spec type() :: atom
+  def type, do: :string
 
-    @spec cast(Integer.t | String.t) :: {atom, String.t}
-    def cast(term)
-    def cast(term) when is_integer(term), do: term |> to_string() |> cast()
-    def cast(term) when is_binary(term), do: {:ok, term}
-    def cast(_), do: :error
+  @spec cast(Integer.t | String.t) :: {atom, String.t}
+  def cast(term)
+  def cast(term) when is_integer(term), do: term |> to_string() |> cast()
+  def cast(term) when is_binary(term), do: {:ok, term}
+  def cast(_), do: :error
 
-    @spec load(String.t) :: {atom, String.t}
-    def load(term) when is_binary(term), do: {:ok, term}
-    def load(_), do: :error
+  @spec load(String.t) :: {atom, String.t}
+  def load(term) when is_binary(term), do: {:ok, term}
+  def load(_), do: :error
 
-    @spec dump(String.t) :: {atom, list(String.t)}
-    def dump(term) when is_binary(term), do: {:ok, term}
-    def dump(_), do: :error
-  end
+  @spec dump(String.t) :: {atom, list(String.t)}
+  def dump(term) when is_binary(term), do: {:ok, term}
+  def dump(_), do: :error
 end
